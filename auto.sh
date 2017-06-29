d72a94f1826f19bf8149f1e951eff07d6c08cd57
Linggih Saputro <linggih.saputro@sci.ui.ac.id>
2017-06-28 23:52:26 -0400

    Updated auto.sh

--- auto.sh
+++ auto.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 echo "---Starting Script---"
 echo "---Generating Vars---"
-ARRAY=(UCS_f10 ACC_f10 UVM_f10 MESO_f10 KICH_f10)
+ARRAY=(BRCA_90)
 for TCGA in "${ARRAY[@]}"
 do
 	INITIAL=$TCGA
-	FILE="filtered10/$INITIAL.txt"
+	FILE="90/$INITIAL.txt"
 	echo "---Making Directory of $INITIAL---"
 	mkdir $INITIAL
 	echo "---ARACNE-AP for $INITIAL---"
-	java -Xmx12G -jar Aracne.jar -e $FILE -o $INITIAL --tfs Taylor_TF.txt --pvalue 1E-8 --seed 1 --calculateThreshold
+	java -Xmx5G -jar Aracne.jar -e $FILE -o $INITIAL --tfs Taylor_TF.txt --pvalue 1E-8 --seed 1 --calculateThreshold
 	echo "---Threshold Calculated---"
 	echo "---Run 100 Reps Bootstraps---"
-	for i in {1..3}	
+	for i in {0..100}	
 	do
-		java -Xmx12G -jar Aracne.jar -e $FILE -o $INITIAL --tfs Taylor_TF.txt --pvalue 1E-8 --seed $i
+		java -Xmx5G -jar Aracne.jar -e $FILE -o $INITIAL --tfs Taylor_TF.txt --pvalue 1E-8 --seed $i
 	done
 	echo "---All Iterations Done!---"
 	echo "---Consolidating---"
-	java -Xmx12G -jar Aracne.jar -o $INITIAL --consolidate
+	java -Xmx5G -jar Aracne.jar -o $INITIAL --consolidate
 	echo "---Done!, check on the network.txt in $INITIAL---"
 done
 
